6252b285e5a6c96edc91596a0217b50fe75734ba
Jesus Molina <mmjesu6@mapfre.com>
2022-05-17 09:13:36 +0200

    Fix: fixing test to run script

--- testScript.sh
+++ testScript.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-OUTPUT= $(echo "Jesus" | bash scrPrueba.sh Jesus)
-if ["${OUTPUT}" = "Encantado de saludarte, Jesus"];then
+OUTPUT= $(echo "Jesus"|bash scrPrueba.sh secreto)
+if ["$OUTPUT" = "Encantado de saludarte, Jesus"];then
     echo "Test superado"
+else 
+    echo "test 1 ha fallado"
     exit 1
 fi 
-echo "Hola "
-OUTPUT= $(echo "secreto" | bash scrPrueba.sh secrecto)
-if ["${OUTPUT}" = "Encontraste el truco!: el primer parámetro del script es: oterces"];then
+OUTPUT= $(echo "secreto"|bash scrPrueba.sh secrecto)
+if ["$OUTPUT" = "Encontraste el truco!: el primer parámetro del script es: oterces"];then
     echo "Test truco superado"
+else 
+    echo "test ha fallado"
     exit 1
 fi 
